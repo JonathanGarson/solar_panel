1eb6cf741e7b48e35c87353f8af47fc4c6a56aaf
Jonathan Garson <garsonjonathan22@gmail.com>
2025-01-25 16:25:30 +0100

    list manufacturer

--- codes/01_generate_data/01_cleaning_TTS.R
+++ codes/01_generate_data/01_cleaning_TTS.R
@@ -9,19 +9,22 @@ library(ggplot2)
 ts = read_parquet(data_raw("TTS.parquet"))
 
 # Data Cleaning -----------------------------------------------------------
-names(ts)
-
 #getting rid of variables concerning the orientation of the solar panel
 cons_column = setdiff(colnames(ts), c("azimuth_1","azimuth_2","azimuth_3","tilt_1","tilt_2","tilt_3"))
 ts[, .SD, .SDcols = cons_column]
 
 #getting rid of observations with no information for: installer, zip, manafucturer, module model, customer segment 
-columns_to_filter <- c("installation_date", "zip_code","installer_name", "module_manufacturer_1", "module_model_1", "technology_module_1","nameplate_capacity_module_1")
-filtered_ts <- ts[apply(ts[, ..columns_to_filter] != "-1", 1, all)]
-filtered_ts = filtered_ts[installation_date != "NaT",]
+string_filter = c("installation_date", "zip_code","installer_name", "module_manufacturer_1", "module_model_1", "technology_module_1")
+num_filter = c("efficiency_module_1", "name_plate_capacity_module_1")
+
+ts_f = ts[apply(ts[, ..string_filter] != "-1", 1, all)]
+ts_f = ts[apply(ts[, ..string_filter] != -1, 1, all)]
+ts_f = ts_f[installation_date != "NaT",]
 
 #adding a year column
-filtered_ts[, year := as.numeric(substr(installation_date, 8, 11))]
-
+ts_f[, year := as.numeric(substr(installation_date, 8, 11))]
+ 
+unique(ts_f$module_manufacturer_1)
+#list of manufacturer to merge
 
 
