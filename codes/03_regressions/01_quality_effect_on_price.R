8cd410eb389768c62bba08bd0c1cbca658a89fe6
JonathanGarson <garsonjonathan22@gmail.com>
2025-03-26 17:50:35 +0100

    first table

--- codes/03_regressions/01_quality_effect_on_price.R
+++ codes/03_regressions/01_quality_effect_on_price.R
@@ -5,7 +5,8 @@ library(arrow)
 library(data.table)
 library(fixest)
 library(modelsummary)
-library(kableExtra)
+library(gt)
+library(flextable)
 
 # Data --------------------------------------------------------------------
 
@@ -26,15 +27,15 @@ run_wave_models <- function(data, quality_var) {
     
     "FE: Year + Installer + Zip" = feols(
       as.formula(paste0("ln_price_w ~ ", quality_var, " | year_quarter + installer_name + zip_code")),
-      cluster = ~zip_code, data = data),
+      cluster = ~state, data = data),
     
     "FE: Year + Module Manufacturer + Zip" = feols(
       as.formula(paste0("ln_price_w ~ ", quality_var, " | year_quarter + module_manufacturer + zip_code")),
-      cluster = ~zip_code, data = data),
+      cluster = ~state, data = data),
     
     "FE: + Module Manufacturer" = feols(
       as.formula(paste0("ln_price_w ~ ", quality_var, " | year_quarter + installer_name + zip_code + module_manufacturer")),
-      cluster = ~zip_code, data = data)
+      cluster = ~state, data = data)
   )
 }
 
@@ -45,25 +46,25 @@ third_wave  <- tts[year_quarter >= "2016Q1" & year_quarter <= "2017Q4"]
 
 # Run all models and name them carefully for each wave and quality measure
 models_1 <- list(
-  "(1)" = run_wave_models(tts, "quality_1")[["No FE"]],
-  "(2)" = run_wave_models(tts, "quality_1")[["FE: Year + Installer + Zip"]],
-  "(3)" = run_wave_models(tts, "quality_1")[["FE: Year + Module Manufacturer + Zip"]],
-  "(4)" = run_wave_models(tts, "quality_1")[["FE: + Module Manufacturer"]],
+  "Quality 1" = list(run_wave_models(tts, "quality_1")[["No FE"]],
+  run_wave_models(tts, "quality_1")[["FE: Year + Installer + Zip"]],
+  run_wave_models(tts, "quality_1")[["FE: Year + Module Manufacturer + Zip"]],
+  run_wave_models(tts, "quality_1")[["FE: + Module Manufacturer"]]),
   
-  "(5)"  = run_wave_models(tts, "quality_2")[["No FE"]],
-  "(6)"= run_wave_models(tts, "quality_2")[["FE: Year + Installer + Zip"]],
-  "(7)" = run_wave_models(tts, "quality_2")[["FE: Year + Module Manufacturer + Zip"]],
-  "(8)"= run_wave_models(tts, "quality_2")[["FE: + Module Manufacturer"]]
+  "Quality 2" = list(run_wave_models(tts, "quality_2")[["No FE"]],
+  run_wave_models(tts, "quality_2")[["FE: Year + Installer + Zip"]],
+  run_wave_models(tts, "quality_2")[["FE: Year + Module Manufacturer + Zip"]],
+  run_wave_models(tts, "quality_2")[["FE: + Module Manufacturer"]])
   )
 
-p_value_1 = fitstat(models_1$`(1)`, type = c("f"))$f$p
-p_value_2 = fitstat(models_1$`(2)`, type = c("f"))$f$p
-p_value_3 = fitstat(models_1$`(3)`, type = c("f"))$f$p
-p_value_4 = fitstat(models_1$`(5)`, type = c("f"))$f$p
-p_value_5 = fitstat(models_1$`(4)`, type = c("f"))$f$p
-p_value_6 = fitstat(models_1$`(6)`, type = c("f"))$f$p
-p_value_7 = fitstat(models_1$`(7)`, type = c("f"))$f$p
-p_value_8 = fitstat(models_1$`(8)`, type = c("f"))$f$p
+p_value_1 = fitstat(models_1$`Quality 1`[[1]], type = c("f"))$f$p
+p_value_2 = fitstat(models_1$`Quality 1`[[2]], type = c("f"))$f$p
+p_value_3 = fitstat(models_1$`Quality 1`[[3]], type = c("f"))$f$p
+p_value_4 = fitstat(models_1$`Quality 1`[[4]], type = c("f"))$f$p
+p_value_5 = fitstat(models_1$`Quality 2`[[1]], type = c("f"))$f$p
+p_value_6 = fitstat(models_1$`Quality 2`[[2]], type = c("f"))$f$p
+p_value_7 = fitstat(models_1$`Quality 2`[[3]], type = c("f"))$f$p
+p_value_8 = fitstat(models_1$`Quality 2`[[4]], type = c("f"))$f$p
 
 add_row = data.frame(
   test = "F-test p-value",
@@ -77,36 +78,10 @@ add_row = data.frame(
   p8 = p_value_8 
 )
 
-models_2 = list(
-"2010 - 2012" = list("(1)"  = run_wave_models(first_wave, "quality_1_ad1")[["No FE"]],
-                  "(2)" = run_wave_models(first_wave, "quality_1_ad1")[["FE: Year + Installer + Zip"]],
-                  "(3)" = run_wave_models(first_wave, "quality_1_ad1")[["FE: + Module Manufacturer"]],
-
-                  "(4)"  = run_wave_models(first_wave, "quality_2_ad1")[["No FE"]],
-                  "(5)"= run_wave_models(first_wave, "quality_2_ad1")[["FE: Year + Installer + Zip"]],
-                  "(6)"= run_wave_models(first_wave, "quality_2_ad1")[["FE: + Module Manufacturer"]]),
-
-"2013 - 2014" =  list("(7)" = run_wave_models(second_wave, "quality_1_ad2")[["No FE"]],
-                   "(8)" = run_wave_models(second_wave, "quality_1_ad2")[["FE: Year + Installer + Zip"]],
-                   "(9)" = run_wave_models(second_wave, "quality_1_ad2")[["FE: + Module Manufacturer"]],
-
-                   "(10)" = run_wave_models(second_wave, "quality_2_ad2")[["No FE"]],
-                   "(11)" = run_wave_models(second_wave, "quality_2_ad2")[["FE: Year + Installer + Zip"]],
-                   "(12)" = run_wave_models(second_wave, "quality_2_ad2")[["FE: + Module Manufacturer"]]),
-
-"2016- 2018" =  list("(13)"  = run_wave_models(third_wave, "quality_1_st")[["No FE"]],
-                   "(14)"  = run_wave_models(third_wave, "quality_1_st")[["FE: Year + Installer + Zip"]],
-                   "(15)"  = run_wave_models(third_wave, "quality_1_st")[["FE: + Module Manufacturer"]],
-
-                   "(16)"  = run_wave_models(third_wave, "quality_2_st")[["No FE"]],
-                   "(17)"  = run_wave_models(third_wave, "quality_2_st")[["FE: Year + Installer + Zip"]],
-                   "(18)"  = run_wave_models(third_wave, "quality_2_st")[["FE: + Module Manufacturer"]])
-)
-
 # Map raw coefficient names to prettier labels
 coef_map <- c(
-  "quality_1" = "Quality 1",
-  "quality_2" = "Quality 2",
+  "quality_1" = "Premium Panels",
+  "quality_2" = "Premium Installations",
   "quality_1_ad1" = "Quality 1",
   "quality_2_ad1" = "Quality 2",
   "quality_1_ad2" = "Quality 1",
@@ -119,17 +94,16 @@ coef_map <- c(
 table1_quality <- modelsummary(
   models_1,
   coef_map = coef_map,
-  statistic = "({std.error})",
-  stars = TRUE,
-  gof_omit = "Adj|AIC|BIC|Log|Within|Pseudo|RMSE",
+  stars = F,
+  shape = "cbind",
+  gof_omit = "Adj|AIC|BIC|Log|Within|Pseudo|RMSE|Std.",
   add_rows = add_row,
-  output = "kableExtra"
+  output = "latex"
 )
+table1_quality_char = as.character(table1_quality)
+writeLines(table1_quality_char, "output/regression/descriptive/table1_quality.tex")
 
-# Apply kableExtra styling and force LaTeX format
-table1_quality <- table1_quality %>% 
-  kable_styling(full_width = FALSE, latex_options = c("hold_position"))
-save_kable(table1_quality, file = "output/regression/descriptive/table1_quality.tex")
+
 
 # Panel B - Quality 2 -----------------------------------------------------
 
